15cd628b1523e4dcfb03f737f6cef262b0d682e7
Aditya Gandhamal <61016383+adityagandhamal@users.noreply.github.com>
2025-03-24 11:55:14 +0530

    remove detectron2 and specify open_clip version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='clip_cam',
-    version='0.1.0',
+    version='0.1.5',
     author='Aditya Gandhamal, Aniruddh Sikdar',
     author_email='',
-    description='A package for visualizing **the prompt-image feature matching** in ViT-based CLIP models, highlighting the alignment between image features and textual prompts.',
+    description='A package for visualizing the prompt-image feature matching in ViT-based CLIP models, highlighting the alignment between image features and textual prompts.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license='MIT',
@@ -17,9 +17,8 @@ setup(
         'einops',
         'opencv-python',
         'matplotlib',
-        'detectron2',
         'Pillow',
-        'open_clip',
+        'open_clip_torch==2.29.0',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
